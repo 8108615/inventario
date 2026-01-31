32b75986da70549c62f647d9df92cde5c16d0aac
8108615 <moralesgilerickfernando7@gmail.com>
2026-01-31 04:36:28 -0400

    Se agregó hasta Categorias video1

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+
+            [   'name' => 'Electronicos',
+                'description' => 'Articulos Electronicos',
+            ],
+            [   'name' => 'Ropa',
+                'description' => 'Articulos de Ropa',
+            ],
+            [   'name' => 'Alimentos',
+                'description' => 'Articulos de Alimentos',
+            ],
+            [   'name' => 'Hogar',
+                'description' => 'Articulos para el hogar',
+            ],
+            [   'name' => 'Juguetes',
+                'description' => 'Articulos de Juguetes',
+            ],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
+    }
+}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+<x-admin-layout
+title="Dashboard | Inventario"
+:breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'href' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Pruebas',
+    ]
+]">
+
+
+
+
+
+
+
+</x-admin-layout>
